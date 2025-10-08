b50142e6afd4dc0296825de453d386ee0ab4dc6a
Juan Cruz Pereira <juancp98@hotmail.com>
2025-10-08 11:04:53 -0300

    feat: Criar ApplicationConfig e configurar um método ModelMapper

--- src/main/java/com/gastospessoaisapi/gastospessoais/domain/service/UserAccountService.java
+++ src/main/java/com/gastospessoaisapi/gastospessoais/domain/service/UserAccountService.java
@@ -1,6 +1,37 @@
 package com.gastospessoaisapi.gastospessoais.domain.service;
 
 
+import com.gastospessoaisapi.gastospessoais.domain.dto.userAccount.UserAccountRequestDto;
+import com.gastospessoaisapi.gastospessoais.domain.dto.userAccount.UserAccountResponseDto;
+
+import java.util.List;
 
 public class UserAccountService implements ICRUDService<UserAccountRequestDto, UserAccountResponseDto> {
+
+
+
+    @Override
+    public List<UserAccountResponseDto> getAll() {
+        return List.of();
+    }
+
+    @Override
+    public UserAccountResponseDto getById(Long id) {
+        return null;
+    }
+
+    @Override
+    public UserAccountResponseDto register(UserAccountRequestDto dto) {
+        return null;
+    }
+
+    @Override
+    public UserAccountResponseDto update(Long id, UserAccountRequestDto dto) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
 }
