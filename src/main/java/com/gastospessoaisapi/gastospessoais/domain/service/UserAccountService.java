85649f08fc169176cecf0d4cd478ce0b16c46cbb
Juan Cruz Pereira <juancp98@hotmail.com>
2025-10-08 14:37:42 -0300

    feat: Criar metodos services getAll e getById para o userAccount

--- src/main/java/com/gastospessoaisapi/gastospessoais/domain/service/UserAccountService.java
+++ src/main/java/com/gastospessoaisapi/gastospessoais/domain/service/UserAccountService.java
@@ -1,23 +1,46 @@
 package com.gastospessoaisapi.gastospessoais.domain.service;
 
-
 import com.gastospessoaisapi.gastospessoais.domain.dto.userAccount.UserAccountRequestDto;
 import com.gastospessoaisapi.gastospessoais.domain.dto.userAccount.UserAccountResponseDto;
+import com.gastospessoaisapi.gastospessoais.domain.model.UserAccount;
+import com.gastospessoaisapi.gastospessoais.domain.repository.UserAccountRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class UserAccountService implements ICRUDService<UserAccountRequestDto, UserAccountResponseDto> {
 
+    private UserAccountRepository userAccountRepository;
+
+    public UserAccountService(UserAccountRepository userAccountRepository) {
+        this.userAccountRepository = userAccountRepository;
+    }
+
+    @Autowired
+    private ModelMapper mapper;
 
 
     @Override
     public List<UserAccountResponseDto> getAll() {
-        return List.of();
+        List<UserAccount> userAccounts = userAccountRepository.findAll();
+
+        return userAccounts.stream()
+        .map(user -> mapper.map(user, UserAccountResponseDto.class))
+                .collect(Collectors.toList());
     }
 
     @Override
     public UserAccountResponseDto getById(Long id) {
-        return null;
+        Optional<UserAccount> optionalUserAccount = userAccountRepository.findById(id);
+
+        if(optionalUserAccount.isEmpty()) {
+        }
+
+        return mapper.map(optionalUserAccount.get(), UserAccountResponseDto.class);
+
     }
 
     @Override
